98ca602964d2da3fb6216e6b812848b216f833cd
Thomas Hansen <thomas@gaiasoul.com>
2019-09-26 11:46:50 +0300

    Adding unit tests

---
+++ magic.lambda.tests/ForEachTests.cs
@@ -0,0 +1,38 @@
+/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System.Linq;
+using Xunit;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.tests.lambda
+{
+    public class ForEachTests
+    {
+        [Slot(Name = "foo2")]
+        public class Foo2Slot : ISlot
+        {
+            public static int ExecutionCount;
+
+            public void Signal(ISignaler signaler, Node input)
+            {
+                ++ExecutionCount;
+            }
+        }
+
+        [Fact]
+        public void ForEach_01()
+        {
+            Common.Evaluate(@".foo1
+   bar1
+   bar2
+   bar3
+for-each:x:../*/.foo1/*
+   foo2");
+            Assert.Equal(3, Foo2Slot.ExecutionCount);
+        }
+    }
+}
