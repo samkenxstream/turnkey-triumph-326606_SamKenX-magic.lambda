b61420aba91c1579f47cf9bb74693f6587f630af
Thomas Hansen <thomas@gaiasoul.com>
2019-10-09 18:22:37 +0300

    Update AsyncTests.cs

--- magic.lambda.tests/AsyncTests.cs
+++ magic.lambda.tests/AsyncTests.cs
@@ -12,19 +12,27 @@ namespace magic.lambda.tests
     public class AsyncTests
     {
         [Fact]
-        public async Task AddChildrenSrc()
+        public async Task AddChildrenSrcAsync()
         {
             var lambda = await Common.EvaluateAsync(@"
 .dest
 wait.add:x:../*/.dest
    .
       foo1:bar1
+      foo2:bar2
+*add:x:../*/.dest
+   .
+      foo1:bar1
       foo2:bar2");
-            Assert.Equal(2, lambda.Children.First().Children.Count());
+            Assert.Equal(4, lambda.Children.First().Children.Count());
             Assert.Equal("foo1", lambda.Children.First().Children.First().Name);
             Assert.Equal("bar1", lambda.Children.First().Children.First().Value);
             Assert.Equal("foo2", lambda.Children.First().Children.Skip(1).First().Name);
             Assert.Equal("bar2", lambda.Children.First().Children.Skip(1).First().Value);
+            Assert.Equal("foo1", lambda.Children.First().Children.Skip(2).First().Name);
+            Assert.Equal("bar1", lambda.Children.First().Children.Skip(2).First().Value);
+            Assert.Equal("foo2", lambda.Children.First().Children.Skip(3).First().Name);
+            Assert.Equal("bar2", lambda.Children.First().Children.Skip(3).First().Value);
         }
     }
 }
