a9862bb224c5b34ef0c3b46e6d7e284f7cf1ed45
Thomas Hansen <thomas@gaiasoul.com>
2021-03-08 12:31:11 +0200

    Adding [format]

---
+++ magic.lambda/change/Format.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System;
+using System.Text;
+using System.Linq;
+using sys = System;
+using System.Globalization;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+using magic.node.extensions.hyperlambda;
+
+namespace magic.lambda.change
+{
+    /// <summary>
+    /// [format] slot allowing you to format some value according to some pattern.
+    /// </summary>
+    [Slot(Name = "format")]
+    public class Format : ISlot
+    {
+        /// <summary>
+        /// Implementation of signal
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal</param>
+        /// <param name="input">Parameters passed from signaler</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            var value = input.GetEx<object>();
+            var pattern = input.Children.First().GetEx<string>();
+            input.Clear();
+            input.Value = string.Format(CultureInfo.InvariantCulture, pattern, value);
+        }
+    }
+}
