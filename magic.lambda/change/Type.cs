c374fd83c9e07cd396118ff9da75777cde82388e
Thomas Hansen <thomas@gaiasoul.com>
2021-03-29 17:37:03 +0300

    [type] slot to retrieve type information about some value

---
+++ magic.lambda/change/Type.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System;
+using System.Text;
+using System.Linq;
+using sys = System;
+using System.Globalization;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+using magic.node.extensions.hyperlambda;
+
+namespace magic.lambda.change
+{
+    /// <summary>
+    /// [type] slot allowing you to retrieve Hyperlambda type information for some specified value.
+    /// </summary>
+    [Slot(Name = "type")]
+    public class Type : ISlot
+    {
+        /// <summary>
+        /// Implementation of signal
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal</param>
+        /// <param name="input">Parameters passed from signaler</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            var value = input.GetEx<object>();
+            var result = Converter.ToString(value);
+            input.Value = result.Item1;
+        }
+    }
+}
