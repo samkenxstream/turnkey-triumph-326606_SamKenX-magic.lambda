ae03dd17b6dacf6d725471d27138459487a3e4f9
Thomas Hansen <thomas@gaiasoul.com>
2022-01-09 09:15:24 +0200

    Implementing [neq] slot

---
+++ magic.lambda/comparison/Neq.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+using System.Threading.Tasks;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda.comparison
+{
+    /// <summary>
+    /// [neq] slot allowing you to compare two values for not equality.
+    /// </summary>
+    [Slot(Name = "neq")]
+    public class Neq : ISlot, ISlotAsync
+    {
+        /// <summary>
+        /// Implementation of signal
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal</param>
+        /// <param name="input">Parameters passed from signaler</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            signaler.Signal("eq", input);
+            input.Value = !input.Get<bool>();
+        }
+
+        /// <summary>
+        /// Implementation of signal
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal</param>
+        /// <param name="input">Parameters passed from signaler</param>
+        /// <returns>An awaitable task.</returns>
+        public async Task SignalAsync(ISignaler signaler, Node input)
+        {
+            await signaler.SignalAsync("eq", input);
+            input.Value = !input.Get<bool>();
+        }
+    }
+}
