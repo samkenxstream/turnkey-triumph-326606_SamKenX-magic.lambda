d2fbfcbe0b4199c8fdedbf111d03ab74b4420cfe
Thomas Hansen <thomas@gaiasoul.com>
2022-01-09 10:04:18 +0200

    Better coverage [whitelist] throws

--- magic.lambda.tests/LogicalTests.cs
+++ magic.lambda.tests/LogicalTests.cs
@@ -3,7 +3,9 @@
  */
 
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
+using magic.node.extensions;
 
 namespace magic.lambda.tests
 {
@@ -12,35 +14,86 @@ namespace magic.lambda.tests
         [Fact]
         public void And_01()
         {
-            var lambda = Common.Evaluate(".foo1:bool:true\nand\n   get-value:x:../*/.foo1\n   .:bool:true");
+            var lambda = Common.Evaluate(@"
+.foo1:bool:true
+and
+   get-value:x:../*/.foo1
+   .:bool:true");
             Assert.Equal(true, lambda.Children.Skip(1).First().Value);
         }
 
         [Fact]
         public void And_02()
         {
-            var lambda = Common.Evaluate(".foo1:bool:true\nand\n   get-value:x:../*/.foo1\n   .:bool:true\n   .:bool:false");
+            var lambda = Common.Evaluate(@"
+.foo1:bool:true
+and
+   get-value:x:../*/.foo1
+   .:bool:true
+   .:bool:false");
             Assert.Equal(false, lambda.Children.Skip(1).First().Value);
+        }
+
+        [Fact]
+        public void AndWhitelist_Throws()
+        {
+            Assert.Throws<HyperlambdaException>(() => Common.Evaluate(@"
+.foo1:bool:true
+whitelist
+   vocabulary
+      set-value
+   .lambda
+      and
+         get-value:x:../*/.foo1
+         .:bool:true"));
+        }
+
+        [Fact]
+        public async Task AndWhitelistAsync_Throws()
+        {
+            await Assert.ThrowsAsync<HyperlambdaException>(async () => await Common.EvaluateAsync(@"
+.foo1:bool:true
+whitelist
+   vocabulary
+      set-value
+   .lambda
+      and
+         get-value:x:../*/.foo1
+         .:bool:true"));
         }
 
         [Fact]
         public void Or_01()
         {
-            var lambda = Common.Evaluate(".foo1:bool:true\nor\n   get-value:x:../*/.foo1\n   .:bool:false");
+            var lambda = Common.Evaluate(@"
+.foo1:bool:true
+or
+   get-value:x:../*/.foo1
+   .:bool:false");
             Assert.Equal(true, lambda.Children.Skip(1).First().Value);
         }
 
         [Fact]
         public void Or_02()
         {
-            var lambda = Common.Evaluate(".foo1:bool:false\nor\n   get-value:x:../*/.foo1\n   .:bool:true\n   .:bool:false");
+            var lambda = Common.Evaluate(@"
+.foo1:bool:false
+or
+   get-value:x:../*/.foo1
+   .:bool:true
+   .:bool:false");
             Assert.Equal(true, lambda.Children.Skip(1).First().Value);
         }
 
         [Fact]
         public void Or_03()
         {
-            var lambda = Common.Evaluate(".foo1:bool:false\nor\n   get-value:x:../*/.foo1\n   .:bool:false\n   .:bool:false");
+            var lambda = Common.Evaluate(@"
+.foo1:bool:false
+or
+   get-value:x:../*/.foo1
+   .:bool:false
+   .:bool:false");
             Assert.Equal(false, lambda.Children.Skip(1).First().Value);
         }
     }
